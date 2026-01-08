fa2055c688276633195cdbe63a3448f2dbb37663
Brady Holt <brady.holt@gmail.com>
2026-01-08 10:35:26 -0600

    Fix error in GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ynab-recurring-charges-finder/',
-  resolve: {
-    alias: {
-      ynab: 'ynab/dist/browser/ynab.js',
+  optimizeDeps: {
+    include: ['ynab'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
     },
   },
 })
